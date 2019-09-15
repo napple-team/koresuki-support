e93e33da9ddf55b8ee1b26b83b5f8d6b4bf472ce
windyakin <windyakin@gmail.com>
2019-09-15 18:57:18 +0900

    :sparkles: 投稿するようにしてみる

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const URI = require('url');
+const { IncomingWebhook } = require('@slack/webhook');
+
+const safeWebhookUrl = process.env.SAFE_CHANNEL_WEBHOOK_URL;
+const nsfwWebhookUrl = process.env.NSFW_CHANNEL_WEBHOOK_URL;
+
+const nsfwChannelId = 'BNEGY0P3R'
 
 exports.koresuki = (req, res) => {
   (async () => {
     const request = req.body.text;
+    const postedUser = req.body.user_id;
     if (!request.match(/https:\/\/twitter\.com\/\w+\/status\/\d+/)) {
       res.status(200).json({
         response_type: 'ephemeral',
@@ -11,7 +17,13 @@ exports.koresuki = (req, res) => {
     } else {
       res.status(200).json({
         response_type: 'ephemeral',
-        text: `\`${request}\` を <#BNEGY0P3R> に投稿しました`,
+        text: `\`${request}\` を <#${nsfwChannelId}> に投稿しました`,
+      });
+      await new IncomingWebhook(safeWebhookUrl).send({
+        text: `<@${postedUser}>さんが <#${nsfwChannelId}> にTwitter URLを投稿しました`,
+      });
+      await new IncomingWebhook(nsfwWebhookUrl).send({
+        text: `<@${postedUser}>さんから: \n${request}`,
       });
     }
   })().then();
