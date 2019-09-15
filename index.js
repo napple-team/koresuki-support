00b39a11b303ffdcbce43d8c0499fbd42de5ba18
windyakin <windyakin@gmail.com>
2019-09-15 19:30:13 +0900

    :sparkles: 表示内容を修正

--- index.js
+++ index.js
@@ -8,23 +8,25 @@ const nsfwChannelId = process.env.NSFW_CHANNEL_ID;
 exports.koresuki = (req, res) => {
   (async () => {
     const request = req.body.text;
-    const postedUser = req.body.user_id;
+    const postedUserName = req.body.user_name;
+
     if (!request.match(/https:\/\/twitter\.com\/\w+\/status\/\d+/)) {
       res.status(200).json({
         response_type: 'ephemeral',
         text: 'URLがTwitterと認識できなかったため処理を中断します',
       });
-    } else {
-      res.status(200).json({
-        response_type: 'ephemeral',
-        text: `\`${request}\` を <#${nsfwChannelId}> に投稿しました`,
-      });
-      await new IncomingWebhook(safeWebhookUrl).send({
-        text: `<@${postedUser}>さんが <#${nsfwChannelId}> にTwitter URLを投稿しました`,
-      });
-      await new IncomingWebhook(nsfwWebhookUrl).send({
-        text: `<@${postedUser}>さんから: \n${request}`,
-      });
+      return;
     }
+
+    res.status(200).json({
+      response_type: 'ephemeral',
+      text: `\`${request}\` を <#${nsfwChannelId}> に投稿しました`,
+    });
+    await new IncomingWebhook(safeWebhookUrl).send({
+      text: `${postedUserName} さんが <#${nsfwChannelId}> に Twitter URL を投稿しました`,
+    });
+    await new IncomingWebhook(nsfwWebhookUrl).send({
+      text: `${postedUserName} さんから: \n${request}`,
+    });
   })().then();
 };
