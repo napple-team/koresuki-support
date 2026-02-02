195157c6a43eaacb2e7d97f183df09d2d159f7ba
windyakin <windyakin@gmail.com>
2026-02-02 22:33:54 +0900

    Hono に置き換え

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const { IncomingWebhook } = require('@slack/webhook');
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { IncomingWebhook } from '@slack/webhook';
+
+const app = new Hono();
 
 const postChannel = {
   safe: {
@@ -11,70 +16,61 @@ const postChannel = {
   },
 };
 
-exports.koresuki = (req, res) => {
+app.use('*', cors());
 
-  // Cross Origin
-  {
-    res.set('Access-Control-Allow-Origin', '*');
-    if (req.method === 'OPTIONS') {
-      // Send response to OPTIONS requests
-      res.set('Access-Control-Allow-Methods', 'GET');
-      res.set('Access-Control-Allow-Headers', 'Content-Type');
-      res.set('Access-Control-Max-Age', '3600');
-      res.status(204).send('');
-      return
-    }
+app.post('/', async (c) => {
+  const body = await c.req.parseBody();
+  const { command, text } = body;
+  const postedUserName = body.user_name;
+
+  if (!/https:\/\/(twitter|x)\.com\/\w+\/status\/\d+/.test(text)) {
+    return c.json({
+      response_type: 'ephemeral',
+      text: 'メッセージ中にTwitterのURLが存在しないため処理を中断します',
+    });
   }
 
-  (async () => {
-    const { command, text } = req.body;
-    const postedUserName = req.body.user_name;
+  let postChannelId = null;
 
-    if (!/https:\/\/(twitter|x)\.com\/\w+\/status\/\d+/.test(text)) {
-      res.status(200).json({
-        response_type: 'ephemeral',
-        text: 'メッセージ中にTwitterのURLが存在しないため処理を中断します',
-      });
-      return;
+  try {
+    switch (command) {
+      case '/koresuki':
+        postChannelId = postChannel.safe.channelId;
+        await new IncomingWebhook(postChannel.safe.webhookUrl).send({
+          text: `${postedUserName} さんから: \n${text}`,
+          unfurl_links: true,
+          unfurl_media: true,
+        });
+        break;
+
+      case '/nsfw':
+        postChannelId = postChannel.nsfw.channelId;
+        await new IncomingWebhook(postChannel.safe.webhookUrl).send({
+          text: `${postedUserName} さんが <#${postChannelId}> に Twitter URL を投稿しました`,
+        });
+        await new IncomingWebhook(postChannel.nsfw.webhookUrl).send({
+          text: `${postedUserName} さんから: \n${text}`,
+        });
+        break;
+
+      default:
+        break;
     }
-
-    let postChannelId = null;
-
-    try {
-      switch (command) {
-        case '/koresuki':
-          postChannelId = postChannel.safe.channelId;
-          await new IncomingWebhook(postChannel.safe.webhookUrl).send({
-            text: `${postedUserName} さんから: \n${text}`,
-            unfurl_links: true,
-            unfurl_media: true,
-          });
-          break;
-
-        case '/nsfw':
-          postChannelId = postChannel.nsfw.channelId;
-          await new IncomingWebhook(postChannel.safe.webhookUrl).send({
-            text: `${postedUserName} さんが <#${postChannelId}> に Twitter URL を投稿しました`,
-          });
-          await new IncomingWebhook(postChannel.nsfw.webhookUrl).send({
-            text: `${postedUserName} さんから: \n${text}`,
-          });
-          break;
-
-        default:
-          break;
-      }
-    } catch (err) {
-      res.status(200).json({
-        response_type: 'ephemeral',
-        text: `投稿に失敗しました\n\`\`\`\n${err}\`\`\``,
-      });
-      return;
-    }
-
-    res.status(200).json({
+  } catch (err) {
+    return c.json({
       response_type: 'ephemeral',
-      text: `下記の内容 を <#${postChannelId}> に投稿しました\n\`\`\`\n${text}\`\`\``,
+      text: `投稿に失敗しました\n\`\`\`\n${err}\`\`\``,
     });
-  })().then();
-};
+  }
+
+  return c.json({
+    response_type: 'ephemeral',
+    text: `下記の内容 を <#${postChannelId}> に投稿しました\n\`\`\`\n${text}\`\`\``,
+  });
+});
+
+const port = parseInt(process.env.PORT || '3000', 10);
+
+serve({ fetch: app.fetch, port }, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`);
+});
